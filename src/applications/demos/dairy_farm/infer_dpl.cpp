81d530e93caef09b2f6a1ac4e5eec5ca1dffe32c
Thompson Liu <mingzhao_liu@163.com>
2021-04-17 15:48:30 -0400

    infer_dpl added put

--- src/applications/demos/dairy_farm/infer_dpl.cpp
+++ src/applications/demos/dairy_farm/infer_dpl.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <thread>
 #include <opencv2/opencv.hpp>
 #include <cppflow/cppflow.h>
 #include <torch/script.h>
@@ -21,34 +22,13 @@ std::string get_description() {
     return MY_DESC;
 }
 
-#define BCS_IMAGE_HEIGHT           (300)
-#define BCS_IMAGE_WIDTH            (300)
-#define BCS_TENSOR_BUFFER_SIZE     (BCS_IMAGE_HEIGHT*BCS_IMAGE_WIDTH*3*sizeof(float))
-#define DPL_CONF_INFER_BCS_MODEL   "CASCADE/bcs_model"
-
-float bsc_infer(void* img_buf, size_t img_size) {
-    /* step 1: load the model */ 
-    cppflow::model model(derecho::getConfString(DPL_CONF_INFER_BCS_MODEL));
-    
-    /* step 2: Load the image & convert to tensor */
-    std::vector<unsigned char> out_buf(img_size);
-    std::memcpy(static_cast<void*>(out_buf.data()),static_cast<const void*>(img_buf),img_size);
-    cv::Mat mat = cv::imdecode(out_buf, cv::IMREAD_COLOR);
-    // resize to desired dimension matching with the model
-    cv::resize(mat, mat, cv::Size(BCS_IMAGE_WIDTH,BCS_IMAGE_HEIGHT));
-    // convert to tensor
-    std::vector<float> bcs_tensor_buf;
-    std::memcpy(static_cast<void*>(bcs_tensor_buf.data()),static_cast<const void*>(mat.data),BCS_TENSOR_BUFFER_SIZE);
-    cppflow::tensor input_tensor(bcs_tensor_buf, {BCS_IMAGE_WIDTH,BCS_IMAGE_HEIGHT,3});
-    input_tensor = cppflow::expand_dims(input_tensor, 0);
-    
-    /* step 3: Predict */
-    cppflow::tensor output = model({{"serving_default_conv2d_5_input:0", input_tensor}},{"StatefulPartitionedCall:0"});
-    float prediction = output.get_data<float>()[0];
-    std::cout << "prediction is: " << std::to_string(prediction) << std::endl;
-    return prediction;
+/* returns [<frame_idx>, <frame_ts>] */
+std::tuple<std::string,std::string> parse_key(const std::string& obj_key) {
+    std::string delim_bs("/"), delim_us("_");
+    std::string frame_key = obj_key.substr(obj_key.rfind(delim_bs) + 1);
+    size_t pos = frame_key.find(delim_us);
+    return std::make_tuple(frame_key.substr(0,pos),frame_key.substr(pos+1));
 }
-
 
 #define K                    (5)			// number of nearest neighbors
 #define DIM		             (128)			// dimension
@@ -130,7 +110,7 @@ public:
         load_labels(label_file);
     }
 
-    int infer(void* img_buf, size_t img_size) {
+    uint32_t infer(void* img_buf, size_t img_size) {
         std::vector<unsigned char> out_buf(img_size);
         std::memcpy(static_cast<void*>(out_buf.data()),static_cast<const void*>(img_buf),img_size);
         cv::Mat mat = cv::imdecode(out_buf, cv::IMREAD_COLOR);
@@ -153,7 +133,7 @@ public:
         for (int i = 0; i < K; i++) {
             std::cout << labels[nnIdx[i]] << "\n";
         }
-        return labels[nnIdx[0]];
+        return static_cast<uint32_t>(labels[nnIdx[0]]);
     } 
 
     ~InferenceEngine() {
@@ -164,7 +144,43 @@ public:
     }
 };
 
+void infer_cow_id(uint32_t* cow_id, void* img_buf, size_t img_size) {
+    InferenceEngine cow_id_ie(derecho::getConfString(DPL_CONF_COWID_MODULE),derecho::getConfString(DPL_CONF_COWID_KNN),derecho::getConfString(DPL_CONF_COWID_LABEL));
+    *cow_id = cow_id_ie.infer(img_buf, img_size);
+}
+
+#define BCS_IMAGE_HEIGHT           (300)
+#define BCS_IMAGE_WIDTH            (300)
+#define BCS_TENSOR_BUFFER_SIZE     (BCS_IMAGE_HEIGHT*BCS_IMAGE_WIDTH*3*sizeof(float))
+#define DPL_CONF_INFER_BCS_MODEL   "CASCADE/bcs_model"
+
+void infer_bcs(float* bcs, void* img_buf, size_t img_size) {
+    /* step 1: load the model */ 
+    cppflow::model model(derecho::getConfString(DPL_CONF_INFER_BCS_MODEL));
+    
+    /* step 2: Load the image & convert to tensor */
+    std::vector<unsigned char> out_buf(img_size);
+    std::memcpy(static_cast<void*>(out_buf.data()),static_cast<const void*>(img_buf),img_size);
+    cv::Mat mat = cv::imdecode(out_buf, cv::IMREAD_COLOR);
+    // resize to desired dimension matching with the model
+    cv::resize(mat, mat, cv::Size(BCS_IMAGE_WIDTH,BCS_IMAGE_HEIGHT));
+    // convert to tensor
+    std::vector<float> bcs_tensor_buf;
+    std::memcpy(static_cast<void*>(bcs_tensor_buf.data()),static_cast<const void*>(mat.data),BCS_TENSOR_BUFFER_SIZE);
+    cppflow::tensor input_tensor(bcs_tensor_buf, {BCS_IMAGE_WIDTH,BCS_IMAGE_HEIGHT,3});
+    input_tensor = cppflow::expand_dims(input_tensor, 0);
+    
+    /* step 3: Predict */
+    cppflow::tensor output = model({{"serving_default_conv2d_5_input:0", input_tensor}},{"StatefulPartitionedCall:0"});
+    float prediction = output.get_data<float>()[0];
+    *bcs = prediction;
+    std::cout << "prediction is: " << std::to_string(prediction) << std::endl;
+}
+
 class DairyFarmInferOCDPO: public OffCriticalDataPathObserver {
+private:
+    mutable std::mutex p2p_send_mutex;
+
     virtual void operator () (const std::string& key_string,
                               persistent::version_t version,
                               const mutils::ByteRepresentable* const value_ptr,
@@ -173,11 +189,36 @@ class DairyFarmInferOCDPO: public OffCriticalDataPathObserver {
                               uint32_t worker_id) override {
         // TODO: do inference and put it to the storage object pool specified by outputs.
         const VolatileCascadeStoreWithStringKey::ObjectType *vcss_value = reinterpret_cast<const VolatileCascadeStoreWithStringKey::ObjectType *>(value_ptr);
-        float bcs = bsc_infer(vcss_value->blob.bytes, vcss_value->blob.size);
-        InferenceEngine cow_id_ie(derecho::getConfString(DPL_CONF_COWID_MODULE),
-                                  derecho::getConfString(DPL_CONF_COWID_KNN),
-                                  derecho::getConfString(DPL_CONF_COWID_LABEL));
-        uint32_t cow_id = cow_id_ie.infer(vcss_value->blob.bytes, vcss_value->blob.size);
+        uint32_t cow_id;
+        float bcs; 
+        std::thread cow_id_inference(infer_cow_id, &cow_id, vcss_value->blob.bytes, vcss_value->blob.size);
+        std::thread bcs_inference(infer_bcs, &bcs, vcss_value->blob.bytes, vcss_value->blob.size);
+        cow_id_inference.join();
+        bcs_inference.join();
+        // put the result to next tier
+        std::string delim("/");
+        std::tuple<std::string, std::string> idx_ts = std::move(parse_key(key_string));
+        std::string obj_value = std::to_string(bcs) + "_" + std::get<1>(idx_ts);
+        for (auto iter = outputs.begin(); iter != outputs.end(); ++iter) {
+            std::string obj_key = iter->first + delim + std::to_string(cow_id);
+            PersistentCascadeStoreWithStringKey::ObjectType obj(obj_key,obj_value.c_str(),obj_value.size());
+            std::lock_guard<std::mutex> lock(p2p_send_mutex);
+            auto* typed_ctxt = dynamic_cast<CascadeContext<VolatileCascadeStoreWithStringKey,PersistentCascadeStoreWithStringKey,TriggerCascadeNoStoreWithStringKey>*>(ctxt);
+            // if true, use trigger put; otherwise, use normal put
+            if (iter->second) {
+                auto result = typed_ctxt->get_service_client_ref().template trigger_put<PersistentCascadeStoreWithStringKey>(obj);
+                // for (auto& reply_future:result.get()) {
+                //     dbg_default_debug("node({}) fulfilled the reply futures",reply_future.first);
+                // }
+            } 
+            else {
+                auto result = typed_ctxt->get_service_client_ref().template put<PersistentCascadeStoreWithStringKey>(obj);
+                for (auto& reply_future:result.get()) {
+                    auto reply = reply_future.second.get();
+                    dbg_default_debug("node({}) replied with version:({:x},{}us)",reply_future.first,std::get<0>(reply),std::get<1>(reply));
+                }
+            }
+        }
     }
 
     static std::shared_ptr<OffCriticalDataPathObserver> ocdpo_ptr;
